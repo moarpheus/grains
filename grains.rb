b31ae24c31a01508fda1040abf804077b081032d
Juris Gribuska <juris.gribuska@bt.com>
2021-06-24 20:35:01 +0300

    completed

--- grains.rb
+++ grains.rb
@@ -1,3 +1,23 @@
+require 'byebug'
+
 class Grains
-  def square number
+  def self.square number
+    raise ArgumentError unless number > 0 && number < 65
+    holder = 1
+    arr = []
+    (1..number).size.times do
+      arr << holder
+      holder = holder * 2
+    end
+    arr.last
+  end
+  def self.total
+    holder = 1
+    arr = []
+    (1..64).size.times do
+      arr << holder
+      holder = holder * 2
+    end
+    arr.inject(0, :+)
+  end
 end
